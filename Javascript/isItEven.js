1dd4af353ad90201eab4f6e9ca5ba10e931f9262
Kyle Marunda <97623896+Elyk-Adnuram@users.noreply.github.com>
2025-02-01 21:28:56 +0200

    readme

--- Javascript/isItEven.js
+++ Javascript/isItEven.js
@@ -1,3 +1,11 @@
+/* In this Kata we are passing a number (n) into a function.
+Your code will determine if the number passed is even (or not).
+The function needs to return either a true or false.
+Numbers may be positive or negative, integers or floats.
+Floats with decimal part non equal to zero are considered UNeven for this kata.
+
+*/
+
 function testEven(n) {
   //if n modulus 2 equals zero, n is an even number
   const result = n % 2 === 0 ? true : false;
