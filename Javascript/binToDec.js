996089571ed83f47772969fa4136c51fcb31aae1
Kyle Marunda <97623896+Elyk-Adnuram@users.noreply.github.com>
2025-03-27 22:33:11 +0200

    binToDec

---
+++ Javascript/binToDec.js
@@ -0,0 +1,5 @@
+// Complete the function which converts a binary number (given as a string) to a decimal number.
+function binToDec(bin) {
+  // parseInt takes str as first arg and radix as 2nd
+  return parseInt(bin, 2);
+}
